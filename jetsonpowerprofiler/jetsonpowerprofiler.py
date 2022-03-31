52e6c6326d0963e2e36ff0b6b2e88729b04b38f8
Ivica <ivica-matic@outlook.com>
2022-03-31 00:40:29 +0000

    Updated jetson power profiler

--- jetsonpowerprofiler/jetsonpowerprofiler.py
+++ jetsonpowerprofiler/jetsonpowerprofiler.py
@@ -5,6 +5,7 @@ _POWER_SENSOR_ENDPOINT = "/sys/bus/i2c/drivers/ina3221x/6-0040/iio:device0//in_p
 _SEQUENCES = []
 _DATA_POINTS = []
 _kill = False
+_sequence = None
 
 
 def send_kill():
@@ -23,15 +24,12 @@ class _DataPoint:
         self.sequence = sequence
 
 
-def measure(sequence=None):
+def measure():
     now = datetime.now()
     file = open(_POWER_SENSOR_ENDPOINT)
     power = file.read()
     power = int(power)
-    data_point = _DataPoint(now, power, sequence)
-    if sequence:
-        if sequence not in _SEQUENCES:
-            _SEQUENCES.append(sequence)
+    data_point = _DataPoint(now, power, _sequence)
     _DATA_POINTS.append(data_point)
 
 
@@ -50,20 +48,36 @@ def get_total_measuring_time():
     return timedelta.total_seconds()
 
 
-def measure_continuous(sequence=None):
+def measure_continuous():
     print("Started measuring")
     global _kill
     while not _kill:
-        measure(sequence)
+        measure()
     _kill = False
     print("Stopped measuring")
+
+
+def set_sequence(sequence):
+    global _sequence
+    _sequence = sequence
+    global _SEQUENCES
+    if sequence:
+        if sequence not in _SEQUENCES:
+            _SEQUENCES.append(sequence)
+
+
+def unset_sequence():
+    global _sequence
+    _sequence = None
 
 
 def clean():
     global _SEQUENCES
     global _DATA_POINTS
+    global _sequence
     _SEQUENCES = []
     _DATA_POINTS = []
+    _sequence = None
 
 
 if __name__ == "__main__":
